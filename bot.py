fa24dcb6da617897fcb92a22247f15e4600fd71f
Dipti <diptimoninath20@gmail.com>
2026-04-24 23:14:55 +0530

    binanceClient is a custom wrapper that simplifies connecting to Binance Testnet and placing futures order

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # bot/client.py
 from binance.client import Client
 
-class BinanceClient:
+class BinanceClient: 
     def __init__(self, api_key: str, api_secret: str, base_url: str):
         self.client = Client(api_key, api_secret, testnet=True)
         self.client.API_URL = base_url
